e70b672dfbdbded89bc77f33db1beb1fdb9718c9
fecet <xiezej@gmail.com>
2024-02-04 16:23:12 +0800

    chore: neovim health

--- lua/neopyter/health.lua
+++ lua/neopyter/health.lua
@@ -5,26 +5,24 @@ local jupyter = require("neopyter.jupyter")
 function M.check()
     local status = jupyter.jupyterlab:is_connecting()
     if status then
-        vim.health.report_info(string.format("Neopyter status: connected"))
-        vim.health.report_info(string.format("address %s", jupyter.jupyterlab.client.address))
-        vim.health.report_start("Jupyter lab")
+        vim.health.info(string.format("Neopyter status: connected"))
+        vim.health.info(string.format("address %s", jupyter.jupyterlab.client.address))
+        vim.health.start("Jupyter lab")
         for _, notebook in pairs(jupyter.jupyterlab.notebook_map) do
             local select_mark = " "
             if jupyter.notebook == notebook then
                 select_mark = "*"
             end
             if notebook:is_attached() then
-                vim.health.report_info(
+                vim.health.info(
                     string.format("%s %s ❤️ %s", select_mark, notebook.local_path, notebook.remote_path)
                 )
             else
-                vim.health.report_info(
-                    string.format("%s %s 💔", select_mark, notebook.local_path, notebook.remote_path)
-                )
+                vim.health.info(string.format("%s %s 💔", select_mark, notebook.local_path, notebook.remote_path))
             end
         end
     else
-        vim.health.report_info(string.format("Neopyter status: disconnected"))
+        vim.health.info(string.format("Neopyter status: disconnected"))
     end
 end
 
