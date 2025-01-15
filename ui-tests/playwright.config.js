d9b0dcc82166605410ec1ae2cd666e1201987f1c
Abao Zhang <abaodoit@gmail.com>
2025-01-15 21:51:09 +0800

    ci: fix integration test issue

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -2,12 +2,12 @@ import process from 'node:process';
 /**
  * Configuration for Playwright using default from @jupyterlab/galata
  */
-import baseConfig from '@jupyterlab/galata/lib/playwright-config';
+import baseConfig from '@jupyterlab/galata/lib/playwright-config.js';
 
 export default {
   ...baseConfig,
   webServer: {
-    command: 'jlpm start',
+    command: 'pnpm start',
     url: 'http://localhost:8888/lab',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
